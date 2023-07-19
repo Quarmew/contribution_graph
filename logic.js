8bb32de20b8ed7cceca342ca1ea8fb9332f09a3e
UE <uefgoo@gmail.com>
2023-07-19 15:59:44 +0500

    class ConGraph: add some functions, comments

--- logic.js
+++ logic.js
@@ -4,8 +4,11 @@ const dayMilliseconds = 24 * 60 * 60 * 1000;
 
 
 class ConGraph {
+    //конструктор
     constructor(objData) {
+        //данные из json
         this.data = objData
+        //пока не используется
         this.colors = { "c0": [], "c1-9": [], "c10-19": [], "c20-29": [], "c30": [] }
         for (let key in this.data) {
             let count = this.data[key]
@@ -18,28 +21,43 @@ class ConGraph {
 
         this.getBoard();
     }
+    // создаёт дни
     getBoard() {
         var currentDate = new Date();
+        // все дни внутри дива
         let board = document.createElement('div');
         for (let i = 0; i > -351.; i--) {
+            //получаем дату = текущая дата - i дней
             let d = new Date().setTime(currentDate.getTime() - (dayMilliseconds * i * -1))
-            console.log(this.getDateFormat(d))
-            if (this.getDateFormat(d) in this.data) {
-                console.log('yes ' + this.data[this.getDateFormat(d)])
-            } else console.log('no')
+            //создаём базовые ячейки
             let cell = document.createElement('div');
-            cell.className = "red";
+            cell.classList.add("cell")
+            let colorClass = "c0";
+            //если в этот день что то было, то:
+            if (this.getDateFormat(d) in this.data) {
+                //определяем цвет через фунцию getColorClassFromCount
+                let count = this.data[this.getDateFormat(d)];
+                colorClass = this.getColorClassFromCount(count)
+            } else colorClass = "c0";
+            //вешаем класс цвета
+            cell.classList.add(colorClass)
+            //указывает дату в айди чтобы можно было найти
+            cell.id = this.getDateFormat(d)
+            //добавить слушатель наведения и выбора
             board.append(cell)
         }
         p.append(board);
     }
+    getCell() { }
     getDateFormat(dNumber) {
+        //передаём число - получаем дату формата из JSON
         let date = new Date(dNumber);
         let month = date.getMonth();
-        if(month.toString().length == 1) month = "0" + month
+        if (month.toString().length == 1) month = "0" + month
         return date.getFullYear() + "-" + month + "-" + date.getDate()
     }
     getColorClassFromCount(count) {
+        //выбираем цвет по числу контрибуций
         if (count > 29) return "c30";
         if (count > 19 && count < 30) return "c20-29";
         if (count > 9 && count < 20) return "c10-19";
